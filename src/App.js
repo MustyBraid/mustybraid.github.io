44cfe4b439f5763c515f322b15ea81d9d738b4f0
Musty Braid <chartiercasey@gmail.com>
2023-04-25 22:42:26 -0400

    Cards, header, and footer are in mostly correct spaces. Wrote a coolmap function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+
+const projects = [
+  {title: "First Project", description: "This is the first project", image: ""},
+  {title: "Second Project", description: "This is the second project", image: ""},
+  {title: "Third Project", description: "This is the third project", image: ""},
+  {title: "Fourth Project", description: "This is the fourth project", image: ""},
+]
+
+function ProjectCards() {
+  const cards = projects.map(project =>
+    <card className="card">
+      <h1>{project.title}</h1>
+      <p>{project.description}</p>
+    </card>)
+  return (
+    <div className='cardContainer'>
+      {cards}
+    </div>
+  )    
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        Casey Chartier
+        <nav>
+            <li>
+              <a href="https://github.com/MustyBraid">Github</a>
+              <a href="https://www.linkedin.com/in/casey-chartier-313b/">LinkedIn</a>
+            </li>
+        </nav>
       </header>
+      <body>
+        <ProjectCards />
+      </body>
+      <footer>
+        <p>Some text here</p>
+      </footer>
     </div>
   );
 }

c0170a70948fc8e25edc2e546d8c4e6690ff86fb
Musty Braid <chartiercasey@gmail.com>
2023-07-25 20:08:25 -0400

    updated icon and title, started fleshing out data more

--- src/data.js
+++ src/data.js
@@ -1,8 +1,9 @@
 const projects = [
-    {title: "A New Leaf", description: "A React app that allows users to own and learn about virtual plants", image: "", descriptionLong: "This is the first project, you currently have it selected"},
-    {title: "Rust Maze-Solver", description: "A project that's currently helping me learn Rust, as well as how to integrate multiple languages in one project", image: "", descriptionLong: "This is the second project, you currently have it selected"},
-    {title: "Teambuilder", description: "A tiny, command-line JS project that uses rxjs to implement observables in JS", image: "", descriptionLong: "This is the third project, you currently have it selected"},
-    {title: "ReadME Generator", description: "This project uses Inquirer to take user input and generate  ReadMEs", image: "", descriptionLong: "This is the fourth project, you currently have it selected"},
+    {title: "A New Leaf", description: "A React app that allows users to own and learn about virtual plants", link: "https://github.com/MustyBraid/a-new-leaf-application", image: "", descriptionLong: "This is the first project, you currently have it selected"},
+    {title: "Javascript Maze Maker", description: "A command-line project that generates and saves JSONs of mazes.", link: "https://github.com/MustyBraid/JS_Maze_Maker", image: "", descriptionLong: "Currenty very early in development, as I'm still learning Rust with much smaller-scale projects"},
+    {title: "Teambuilder", description: "A tiny, command-line JS project that uses rxjs to implement observables in JS", link: "https://github.com/MustyBraid/TeamBuilder", image: "", descriptionLong: "This is the third project, you currently have it selected"},
+    {title: "ReadME Generator", description: "This project uses Inquirer to take user input and generate ReadMEs", link: "https://github.com/MustyBraid/ReadMEGenerator", image: "", descriptionLong: "This is the fourth project, you currently have it selected"},
+    {title: "Rust Maze-Solver", description: "A project that's currently helping me learn Rust, as well as how to integrate multiple languages in one project", link: "https://github.com/MustyBraid/rust_maze_solver", image: "", descriptionLong: "Currenty very early in development, as I'm still learning Rust with much smaller-scale projects"},
   ]
   
   const navData = [
